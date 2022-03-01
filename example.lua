d706e2d37a6aa47ebafe5c3b77c716e60b6d1118
eqq-evil <99731961+eqq-evil@users.noreply.github.com>
2022-03-01 21:39:22 +0100

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,4 @@
+----- docs -> https://raw.githubusercontent.com/eqq-evil/splash_rblx/main/docs.txt
+
+local Splash = loadstring(game:HttpGet'https://raw.githubusercontent.com/eqq-evil/splash_rblx/main/Splash.lua')()
+Splash.new('Example', 'https://raw.githubusercontent.com/eqq-evil/splash_rblx/main/HelloWorld.lua')
